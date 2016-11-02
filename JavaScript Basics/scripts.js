239000cfa5c3473c2dff9d98b536123dd36cdeb6
Bradley Biehl <bradley.biehl@gmail.com>
2016-11-02 10:01:35 -0500

    could be JS training material

---
+++ JavaScript Basics/scripts.js
@@ -0,0 +1,4 @@
+var adjective = prompt("type an adjective");
+var verb = prompt("type a verb");
+alert("you're done");
+document.write = adjective + verb
---
+++ JavaScript Basics/story.js
@@ -0,0 +1,5 @@
+var adjective = prompt("type an adjective");
+var verb = prompt("type a verb");
+var noun = prompt("type a noun");
+alert("you're done");
+document.write("<h2>"+ adjective + " " + noun + " " + verb +"</h2>");
