7e985d7b0c04a444bb50d698702a0f979bd6f757
Bradley Biehl <bradley.biehl@gmail.com>
2016-11-17 19:25:31 -0600

    better refactoring

--- Loop-Arrays-Objects/js/refactor.js
+++ Loop-Arrays-Objects/js/refactor.js
@@ -1,17 +1,28 @@
 // Refactoring
-// DRY it out.
+// DRY it out better!
 
 var html = '';
-var red;
-var green;
-var blue;
 var rgbColor;
 
+function randomRGB() {
+  return Math.floor(Math.random() * 256 );
+}
+
+function randomColor() {
+  var color = 'rgb(';
+  color += randomRGB() + ',';
+  color += randomRGB() + ',';
+  color += randomRGB() + ')';
+  return color;
+}
+
+function print(message) {
+  document.write(message);
+}
+
 for ( var i = 1; i <= 100; i += 1) {
-  red = Math.floor(Math.random() * 256 );
-  green = Math.floor(Math.random() * 256 );
-  blue = Math.floor(Math.random() * 256 );
-  rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+  rgbColor = randomColor();
   html += '<div style="background-color:' + rgbColor + '"></div>';
 }
-document.write(html);
+
+print(html);
