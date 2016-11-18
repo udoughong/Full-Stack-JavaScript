c8205912ae065703cb71b1387dbfc5aead8be498
Bradley Biehl <bradley.biehl@gmail.com>
2016-11-18 09:14:47 -0600

    iterating through an array via the for loop

--- Loop-Arrays-Objects/js/playlist.js
+++ Loop-Arrays-Objects/js/playlist.js
@@ -1,5 +1,26 @@
-var playlist = [];  // an array literal
-playlist.push('Mother', 'She Rides', 'Twist of Cain', 'Five Finger Crawl');
-playlist.unshift('Sacrifice', '13');
+var playlist = [
+  'Mother', 
+  'She Rides', 
+  'Twist of Cain', 
+  '13'
+];
+
+function print(message) {
+  document.write(message);
+}
+
+function printList(list) {
+  var listHTML = '<ol>';
+  for ( var i = 0; i < list.length; i+=1 ) {
+    listHTML += '<li>' + list[i] + '</li>';
+  }
+  listHTML += '</ol>';
+  print(listHTML);
+}
 
 printList(playlist);
+
+// 1. Mother
+// 2. She Rides
+// 3. Twist of Cain
+// 4. 13
