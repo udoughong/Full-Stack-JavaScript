f132419cb70c97f1bdd5e3093b3b2be4218f25f1
Bradley Biehl <bradley.biehl@gmail.com>
2016-11-18 08:52:50 -0600

    adding and removing objects from arrays

---
+++ Loop-Arrays-Objects/js/playlist.js
@@ -0,0 +1,5 @@
+var playlist = [];  // an array literal
+playlist.push('Mother', 'She Rides', 'Twist of Cain', 'Five Finger Crawl');
+playlist.unshift('Sacrifice', '13');
+
+printList(playlist);
--- Loop-Arrays-Objects/js/shopping.js
+++ Loop-Arrays-Objects/js/shopping.js
@@ -2,4 +2,9 @@ var shoppingList = [
   "beer", 
   "more beer", 
   "whiskey"
-  ];
+];
+
+console.log(shoppingList[0]); // beer
+console.log(shoppingList[1]); // more beer
+console.log(shoppingList[2]); // whiskey
+console.log(shoppingList[3]); // undefined
