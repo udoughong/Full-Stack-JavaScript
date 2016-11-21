06eb4b2e188cce2e76a24907fd4dc2de573bf89b
Bradley Biehl <bradley.biehl@gmail.com>
2016-11-21 11:23:14 -0600

    accessing object properties

--- Loop-Arrays-Objects/js/object.js
+++ Loop-Arrays-Objects/js/object.js
@@ -5,3 +5,24 @@ var person = {
   awesomePerformer: true,
   talents: ['Yacht Rock', 'Soundtracks', 'Smoothness']
 };
+
+function print(message) {
+  var div = document.getElementById('output');
+  div.innerHTML = message;
+}
+
+var message = '<p>Hello, my name is ' + person.name + '.</p>';
+message += '<p>I live in the ' + person.country + '.</p>';
+message += '<p>I am ' + person.age + ' years old.</p>';
+person.name = 'Skunk Baxter';
+person.age += -30;
+message += '<p>But, I wish my name was ' + person.name + ' and I was ' + person.age + ' years old.</p>';
+message += '<p>I have ' + person.talents.length + ' main talents, ';
+message += person.talents.join(', ') + '.</p>';
+print(message);
+
+// Hello, my name is Kenny Loggins.
+// I live in the USA.
+// I am 65 years old.
+// But, I wish my name was Skunk Baxter and I was 35 years old.
+// I have 3 main talents, Yacht Rock, Soundtracks, Smoothness.
