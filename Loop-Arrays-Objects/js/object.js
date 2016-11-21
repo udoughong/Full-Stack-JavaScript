992ac381dd67eeeec8efe1c88189df042bcf9256
Bradley Biehl <bradley.biehl@gmail.com>
2016-11-21 11:40:51 -0600

    using the 'for in' Loop through an Object's properties

--- Loop-Arrays-Objects/js/object.js
+++ Loop-Arrays-Objects/js/object.js
@@ -3,26 +3,25 @@ var person = {
   country: 'USA',
   age: 65,
   awesomePerformer: true,
-  talents: ['Yacht Rock', 'Soundtracks', 'Smoothness']
+  skills: ['Yacht Rock', 'Soundtracks', 'Smoothness']
 };
 
-function print(message) {
-  var div = document.getElementById('output');
-  div.innerHTML = message;
+for ( banana in person ) {
+  console.log(banana, ': ', person[banana]);
 }
 
-var message = '<p>Hello, my name is ' + person.name + '.</p>';
-message += '<p>I live in the ' + person.country + '.</p>';
-message += '<p>I am ' + person.age + ' years old.</p>';
-person.name = 'Skunk Baxter';
-person.age += -30;
-message += '<p>But, I wish my name was ' + person.name + ' and I was ' + person.age + ' years old.</p>';
-message += '<p>I have ' + person.talents.length + ' main talents, ';
-message += person.talents.join(', ') + '.</p>';
-print(message);
+// name :  Kenny Loggins
+// country :  USA
+// age :  65
+// awesomePerformer :  true
+// skills :  ["Yacht Rock", "Soundtracks", "Smoothness"]
 
-// Hello, my name is Kenny Loggins.
-// I live in the USA.
-// I am 65 years old.
-// But, I wish my name was Skunk Baxter and I was 35 years old.
-// I have 3 main talents, Yacht Rock, Soundtracks, Smoothness.
+for ( potato in person ) {
+  console.log(potato, ': ', person["country"]);
+}
+
+// name :  USA
+// country :  USA
+// age :  USA
+// awesomePerformer :  USA
+// skills :  USA
