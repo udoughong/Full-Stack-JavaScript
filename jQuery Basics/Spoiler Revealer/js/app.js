7c413162e13da09dc6b02963d106bae7709b04a6
Bradley Biehl <bradley.biehl@gmail.com>
2016-11-23 10:41:15 -0600

    spoiler revealer

--- jQuery Basics/Spoiler Revealer/js/app.js
+++ jQuery Basics/Spoiler Revealer/js/app.js
@@ -1,7 +1,27 @@
 /* Star Wars Spoilers
 
-1. Prevent spoilerphobes from seeing spoilers.
-
-2. Hide spoilers and reveal them through user interaction.
+Goals: 
+Prevent spoilerphobes from seeing spoilers.
+Hide spoilers and reveal them through user interaction.
 
 */
+
+// 1. Hide spoiler.
+$(".spoiler span").hide();
+// 2. Add a button.
+  // http://api.jquery.com/category/manipulation/
+  // http://api.jquery.com/append/
+$(".spoiler").append("<button>Reveal Spoiler!</button>");
+// 3. When button is pressed...
+  // http://api.jquery.com/category/events/mouse-events/
+  // http://api.jquery.com/click/
+$("button").click(function(){
+  // 3.1 Show the spoiler next to the button clicked.
+    // http://api.jquery.com/category/traversing/
+    // http://api.jquery.com/prev/
+  $(this).prev().show();
+  // 3.2 Hide the button that was clicked.
+    // http://api.jquery.com/category/manipulation/dom-removal/
+    // http://api.jquery.com/remove/
+  $(this).remove();
+});
