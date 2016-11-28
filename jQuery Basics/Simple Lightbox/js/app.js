1a431614860d0b777f625fbfa5f266860aad8f66
Bradley Biehl <bradley.biehl@gmail.com>
2016-11-28 17:28:18 -0600

    part 3

--- jQuery Basics/Simple Lightbox/js/app.js
+++ jQuery Basics/Simple Lightbox/js/app.js
@@ -2,6 +2,11 @@
 // Solution: Create an overlay with the large image - lightbox.
 
 var $overlay = $('<div id="overlay"></div>');
+var $image = $("<img>");
+
+// Add image to overlay.
+$overlay.append($image);
+
 // Add the overlay.
 $("body").append($overlay);
   // An image.
@@ -10,20 +15,17 @@ $("body").append($overlay);
 // Capture the click event on a link to an image.
 $("#imageGallery a").click(function(event){
   // Prevent user from going to the deadend.
-  // http://api.jquery.com/category/events/
-  // http://api.jquery.com/event.preventDefault/
   event.preventDefault();
-  // Check the console for href string
-  // http://api.jquery.com/category/attributes/
-  // http://api.jquery.com/attr/
-  var href = $(this).attr("href");
-  console.log(href);
+  var imageLocation = $(this).attr("href");
+  // Update the overlay with the linked image.
+  $image.attr("src", imageLocation);
   // Show the overlay.
   $overlay.show();
-  // Update the overlay with the linked image.
   // Get child's alt atribute and set caption.  
 });
 
-
 // When overlay is clicked.
+$overlay.click(function(){
   // Hide the overlay.
+  $(this).hide();
+});
