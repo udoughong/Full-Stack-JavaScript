3f9a0962917351bc7a312e24276bd037a0ef9f4f
Bradley Biehl <bradley.biehl@gmail.com>
2016-11-29 10:29:13 -0600

    prep and planning

---
+++ jQuery Basics/Mobile Dropdown Menu/js/app.js
@@ -0,0 +1,15 @@
+// Problem: Nav menu is not responsive for mobile.
+// Solution: Create a bootstrap-like navigation.
+
+// Create a select and append to menu.
+// Cycle over menu links.
+  // Create an option.
+  // Option's value is the href.
+  // Option's text is the text of the link.
+  // Append option to select.
+// Create button.
+// Bind click to button.
+  // Go to the select's location.
+// Modify css to hide links on small widths and show button and select.
+// Also hide select and button on larger widths, while showing links.
+// Highlight backgrounds of selected options (depending on current page).
