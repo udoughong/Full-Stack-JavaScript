b1f8248a6ae8e2cc69415a2d336161cf1a7656d1
Bradley Biehl <bradley.biehl@gmail.com>
2016-12-09 12:01:35 -0600

    Using CSS Queries to Select Page Elements

--- JavaScript and the DOM/js/app.js
+++ JavaScript and the DOM/js/app.js
@@ -3,3 +3,16 @@ const myList = document.getElementsByTagName("li");
 for (let i = 0; i < myList.length; i += 1){
   myList[i].style.color = "purple";
 }
+
+const errorNotPurple = document.querySelectorAll(".error-not-purple");
+
+for (let i = 0; i < errorNotPurple.length; i += 1){
+  errorNotPurple[i].style.color = "red";
+}
+
+// make striped table
+const striped = document.querySelectorAll("li:nth-child(odd)");
+
+for (let i = 0; i < striped.length; i += 1){
+  striped[i].style.backgroundColor = "lightgray";
+}
