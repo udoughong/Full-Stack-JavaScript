6b481448de9cd269a03cb625abc5fe59f6d4a9ee
Bradley Biehl <bradley.biehl@gmail.com>
2016-12-09 17:16:44 -0600

    textContent and innerHTML

--- JavaScript and the DOM/js/app.js
+++ JavaScript and the DOM/js/app.js
@@ -1,18 +1,7 @@
-const myList = document.getElementsByTagName("li");
+const input = document.querySelector("input");
+const p = document.querySelector("p.description");
+const button = document.querySelector("button");
 
-for (let i = 0; i < myList.length; i += 1){
-  myList[i].style.color = "purple";
-}
-
-const errorNotPurple = document.querySelectorAll(".error-not-purple");
-
-for (let i = 0; i < errorNotPurple.length; i += 1){
-  errorNotPurple[i].style.color = "red";
-}
-
-// make striped table
-const striped = document.querySelectorAll("li:nth-child(odd)");
-
-for (let i = 0; i < striped.length; i += 1){
-  striped[i].style.backgroundColor = "lightgray";
-}
+button.addEventListener("click", () => {
+  p.innerHTML = input.value + ":";
+});
