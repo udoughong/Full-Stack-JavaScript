54b5cf4d3ea68d071e95bc853bb0f86d250d26c9
Bradley Biehl <bradley.biehl@gmail.com>
2016-12-09 20:28:51 -0600

    adding and removing nodes in the DOM

--- JavaScript and the DOM/js/app.js
+++ JavaScript and the DOM/js/app.js
@@ -1,8 +1,11 @@
 const toggleList = document.querySelector("#toggleList");
-const listDiv = document.querySelector(".list");
-const input = document.querySelector("input.description");
-const p = document.querySelector("p.description");
-const button = document.querySelector("button.description");
+const listDiv = document.querySelector("div.list");
+const descriptionInput = document.querySelector("input.description");
+const descriptionParagraph = document.querySelector("p.description");
+const descriptionButton = document.querySelector("button.description");
+const addItemInput = document.querySelector("input.addItemInput");
+const addItemButton = document.querySelector("button.addItemButton");
+const removeItemButton = document.querySelector("button.removeItemButton");
 
 toggleList.addEventListener("click", () => {
   if (listDiv.style.display == "none") {
@@ -14,6 +17,21 @@ toggleList.addEventListener("click", () => {
   }
 });
 
-button.addEventListener("click", () => {
-  p.innerHTML = input.value + ":";
+descriptionButton.addEventListener("click", () => {
+  descriptionParagraph.innerHTML = descriptionInput.value + ":";
+  descriptionInput.value = ""; // Clears the input.
+});
+
+addItemButton.addEventListener("click", () => {
+  let ul = document.querySelector("ul.list");
+  let li = document.createElement("li");
+  li.innerHTML = addItemInput.value;
+  ul.appendChild(li);
+  addItemInput.value = ""; // Clears the input.
+});
+
+removeItemButton.addEventListener("click", () => {
+  let ul = document.querySelector("ul.list");
+  let li = document.querySelector("li:last-child");
+  ul.removeChild(li);
 });
