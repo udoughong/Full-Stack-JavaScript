77ca56c8da434a3745f84d1fabe3394339430ff6
Bradley Biehl <bradley.biehl@gmail.com>
2016-12-09 19:32:38 -0600

    toggle hide elements on a page

--- JavaScript and the DOM/js/app.js
+++ JavaScript and the DOM/js/app.js
@@ -1,6 +1,18 @@
-const input = document.querySelector("input");
+const toggleList = document.querySelector("#toggleList");
+const listDiv = document.querySelector(".list");
+const input = document.querySelector("input.description");
 const p = document.querySelector("p.description");
-const button = document.querySelector("button");
+const button = document.querySelector("button.description");
+
+toggleList.addEventListener("click", () => {
+  if (listDiv.style.display == "none") {
+    toggleList.innerHTML = "Hide List";
+    listDiv.style.display = "block";
+  } else {
+    toggleList.innerHTML = "Show List";
+    listDiv.style.display = "none";    
+  }
+});
 
 button.addEventListener("click", () => {
   p.innerHTML = input.value + ":";
